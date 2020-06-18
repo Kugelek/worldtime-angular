2d33b333815f3ad16322ddb89ae4422424af4e7e
Kugelek <poblocki.jakub@gmail.com>
2020-06-18 18:26:20 +0200

    final refac

--- world-time-angular/src/app/components/timer/timer.component.ts
+++ world-time-angular/src/app/components/timer/timer.component.ts
@@ -25,39 +25,40 @@ export class TimerComponent implements OnInit {
   updateTime(timeZone: string): void {
     this.setComponentData(timeZone, false);
   }
+
+  //fetch observable from API, set component fields
   setComponentData(timeZone: string, shouldStartLocalTimer: boolean): void {
-    this.timeService.fetchTime(timeZone).subscribe(fetchedTime => {
-      this.timeObj = fetchedTime;
-      this.timeZone = this.timeObj.timezone;
-      [this.hours, this.minutes, this.seconds] = [...this.timeObj.datetime.split('T')[1].split(':')];
-      this.seconds = this.seconds.slice(0, 5);
-      if (shouldStartLocalTimer)
-        this.localUpdateTime();
-    });
+    this.timeService
+      .fetchTime(timeZone)
+      .subscribe(fetchedTime => {
+        this.timeObj = fetchedTime;
+        this.timeZone = this.timeObj.timezone;
+        [this.hours, this.minutes, this.seconds] = [...this.timeObj.datetime.split('T')[1].split(':')];
+        this.seconds = this.seconds.slice(0, 5);
+        if (shouldStartLocalTimer)
+          this.localUpdateTime();
+      });
   }
 
   localUpdateTime(): void {
     this.interval = setInterval(() => {
       this.seconds = parseInt(this.seconds) + 1;
-      if (this.seconds < 10) this.seconds = `0${this.seconds.toString()}`;
-
       if (this.seconds == 60) {
         this.seconds = 0;
-        if (this.seconds < 10) this.seconds = `0${this.seconds.toString()}`;
         this.minutes = parseInt(this.minutes) + 1;
-        if (this.minutes < 10) this.minutes = `0${this.minutes.toString()}`;
 
         if (this.minutes == 60) {
           this.minutes = 0;
-          if (this.minutes < 10) this.minutes = `0${this.minutes.toString()}`;
           this.hours = parseInt(this.hours) + 1;
-          if (this.hours < 10) this.hours = `0${this.hours.toString()}`;
-          if (this.hours == 24) {
+
+          if (this.hours == 24)
             this.hours = 0;
-            if (this.hours < 10) this.hours = `0${this.hours.toString()}`;
-          }
         }
       }
+      //zero prefix added to 1digit parts
+      [this.hours, this.minutes, this.seconds] = [...[this.hours, this.minutes, this.seconds].map(timePart =>
+        timePart.toString().length === 1 ? `0${timePart}` : `${timePart}`)];
+
     }, 1000);
   }
 
