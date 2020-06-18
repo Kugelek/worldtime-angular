e3ffe12e3380714db8615c5277cf83c2eb1e796d
Kugelek <poblocki.jakub@gmail.com>
2020-06-18 17:37:30 +0200

    refac1

--- world-time-angular/src/app/components/timer/timer.component.ts
+++ world-time-angular/src/app/components/timer/timer.component.ts
@@ -8,11 +8,11 @@ import { TimeService } from '../../services/time.service';
 })
 export class TimerComponent implements OnInit {
   timeObj;
-  timeDate;
+  timeDate: string;
   hours;
   minutes;
   seconds;
-  timeZone;
+  timeZone: string;
   interval;
 
   constructor(private timeService: TimeService) {
@@ -23,41 +23,52 @@ export class TimerComponent implements OnInit {
       this.timeObj = fetchedTime;
       this.timeDate = this.timeObj.datetime;
       this.timeZone = this.timeObj.timezone;
-      console.log(this.timeObj);
       [this.hours, this.minutes, this.seconds] = [...this.timeDate.split('T')[1].split(':')];
       this.seconds = this.seconds.slice(0, 5);
       this.localUpdateTime();
     });
   }
 
-  updateTime(timeZone) {
+  updateTime(timeZone: string): void {
     this.timeService.fetchTime(timeZone).subscribe(fetchedTime => {
       this.timeObj = fetchedTime;
       this.timeDate = this.timeObj.datetime;
       this.timeZone = this.timeObj.timezone;
-      console.log(this.timeObj);
       [this.hours, this.minutes, this.seconds] = [...this.timeDate.split('T')[1].split(':')];
       this.seconds = this.seconds.slice(0, 2);
     });
   }
 
-  localUpdateTime() {
+  localUpdateTime(): void {
     this.interval = setInterval(() => {
       this.seconds = parseInt(this.seconds) + 1;
+      if (this.seconds < 10) this.seconds = `0${this.seconds.toString()}`;
+
 
       if (this.seconds == 60) {
         this.seconds = 0;
         this.minutes = parseInt(this.minutes) + 1;
+
+        if (this.minutes == 60) {
+          this.minutes = 0;
+          this.hours = parseInt(this.hours) + 1;
+          if (this.hours == 24) {
+            this.hours = 0;
+            if (this.hours < 10)
+              this.hours = `0${this.hours.toString()}`;
+          }
+          if (this.minutes < 10) {
+            this.minutes = `0${this.minutes.toString()}`;
+          }
+        }
+
       }
-      if (this.minutes == 60) {
-        this.minutes = 0;
-        this.hours = parseInt(this.hours) + 1;
-      }
-      if (this.hours == 24) {
-        this.hours = 0;
-      }
+
     }, 1000);
   }
+
+
+
 }
 
 
